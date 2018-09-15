9a75b20973c861c96051c03004eecd47d8bfec65
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-09-15 02:27:42 -0300

    Choose file box

--- src/components/UI/Button/BaseButton/BaseButton.js
+++ src/components/UI/Button/BaseButton/BaseButton.js
@@ -8,9 +8,11 @@ const BaseButton = (props) => {
     styles.push('confirm')
   else if(props.type === 'cancel')
     styles.push('cancel')
+  else if(props.type === 'choose-file')
+    styles.push('choose-file')
 
   return (
-    <a className={styles.join(' ')}>
+    <a className={styles.join(' ')} onClick={props.click}>
       {props.children}
     </a>
   )
--- src/components/UI/Button/ChooseFile/ChooseFile.js
+++ src/components/UI/Button/ChooseFile/ChooseFile.js
@@ -1,15 +1,29 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, Component} from 'react'
 import BaseButton from '../BaseButton/BaseButton'
 
-const ChooseFile = () => {
+class ChooseFile extends Component {
 
-  return (
-    <Fragment>
-      <input type="file" id="real-file" hidden="hidden"/>
-      <BaseButton>Selecione arquivo</BaseButton>
-    </Fragment>
-    
-  )
+  render() {
+
+    return (
+      <Fragment>
+        <input type="file" id="real-file-btn" hidden="hidden" onChange={this.onChangeFileHandler}/>
+        <BaseButton type="choose-file" click={this.onClickHandler}>Selecione arquivo</BaseButton>
+      </Fragment>
+    )
+  }  
+
+  onClickHandler = () => {
+    const realFileBtn = document.getElementById("real-file-btn").click()
+    console.log(realFileBtn)
+  }
+
+  onChangeFileHandler = () => {
+    const realFileBtn = document.getElementById("real-file-btn")
+    if(realFileBtn.value)
+      console.log(realFileBtn.value)
+  }
+
 }
 
 export default ChooseFile
