87c36ea7e0adf6ba2a450ab61b4150214fe555ab
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-09-15 22:19:04 -0300

    Add restrictions for the input file

--- src/components/UI/DropArea/DropArea.js
+++ src/components/UI/DropArea/DropArea.js
@@ -6,7 +6,10 @@ class DropArea extends Component {
 
   render() {
     return (
-      <ReactDropzone className="drop-area" onDrop={this.onDropHandler}>
+      <ReactDropzone className="drop-area" 
+                     onDrop={this.onDropHandler}
+                     multiple={false}
+                     accept="application/pdf">
         <h1 className="drop-area__title">Arraste o arquivo</h1>
         <p className="drop-area__p">ou</p>
         <p className="drop-area__p">clique dentro desta área</p>
