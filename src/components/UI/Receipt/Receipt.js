5a869dad4c75d3608330f014a67f937551382e97
Phe0 <pheofo@gmail.com>
2018-09-19 14:21:04 -0300

    Add Receipt and ReceiptCompare components and link with the route
    Co-authored-by: Pedro Féo <pheofo@gmail.com>
    Co-authored-by: youssef-md <emaildeyoussefmuhamad@gmail.com>

---
+++ src/components/UI/Receipt/Receipt.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Receipt.scss'
+
+const Receipt = (props) => (
+    <div>
+        <img className="receipt" src="#" />
+        <h1>aaaaaa</h1>
+    </div>
+)
+
+export default Receipt
