751eedc4bc85655ce44c0e3a16bfd2684c3b7a09
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-09-18 23:54:25 -0300

    Remove state from ReceiptAdder

--- src/components/Receipt/ReceiptAdder/ReceiptAdder.js
+++ src/components/Receipt/ReceiptAdder/ReceiptAdder.js
@@ -1,22 +1,18 @@
-import React,{Component} from 'react'
+import React from 'react'
 import './ReceiptAdder.scss'
 import BaseButton from '../../UI/Button/BaseButton/BaseButton'
 import DropArea from '../../UI/DropArea/DropArea'
-class ReceiptAdder extends Component {
 
-  render() {
-    return (
-      <section className="receipt-adder">
-        <DropArea />
-        <div className="receipt-adder__footer">
-          <BaseButton type="cancel">Cancelar</BaseButton>
-          <BaseButton type="confirm">Confirmar</BaseButton>
-        </div>
-      </section>
-    )
-  }
-
-
+const ReceiptAdder = () => {
+  return (
+    <section className="receipt-adder">
+      <DropArea />
+      <div className="receipt-adder__footer">
+        <BaseButton type="cancel">Cancelar</BaseButton>
+        <BaseButton type="confirm">Confirmar</BaseButton>
+      </div>
+    </section>
+  )
 }
 
 export default ReceiptAdder
