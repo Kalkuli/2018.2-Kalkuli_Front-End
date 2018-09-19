0c9260de385fef206ac3c6ebb3787a2662f160ba
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-09-19 00:17:33 -0300

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import Modal from './components/UI/Modal/Modal'
-import ReceiptAdder from './components/Receipt/ReceiptAdder/ReceiptAdder'
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <Modal>
-        <ReceiptAdder/>
-      </Modal>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact render={() => <h1>Home</h1>}/>    
+          <Route path='/send-receipt' render={() => <h1>send receipt</h1>} />
+          <Route path='/compare-data-with-receipt' render={() => <h1>compare data</h1>} />
+          <Route path='/edit-receipt-data' render={() => <h1>edit receipt data</h1>} />
+          <Route path='/list-all-receipts' render={() => <h1>list all receipts</h1>} />
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
