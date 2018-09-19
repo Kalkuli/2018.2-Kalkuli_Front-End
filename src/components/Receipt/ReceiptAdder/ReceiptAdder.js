f75b47d843bb09859181d6037874aa83aee73a42
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-09-19 00:32:00 -0300

    Handle the confirm button event and redirects the page

--- src/components/Receipt/ReceiptAdder/ReceiptAdder.js
+++ src/components/Receipt/ReceiptAdder/ReceiptAdder.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './ReceiptAdder.scss'
 import BaseButton from '../../UI/Button/BaseButton/BaseButton'
 import DropArea from '../../UI/DropArea/DropArea'
 import Modal from '../../UI/Modal/Modal'
 
-const ReceiptAdder = () => {
-  return (
-    <Modal>
-      <section className="receipt-adder">
-        <DropArea />
-        <div className="receipt-adder__footer">
-          <BaseButton type="cancel">Cancelar</BaseButton>
-          <BaseButton type="confirm">Confirmar</BaseButton>
-        </div>
-      </section>
-    </Modal>
-  )
+class ReceiptAdder extends Component {
+
+  render() {
+
+    return (
+      <Modal>
+        <section className="receipt-adder">
+          <DropArea />
+          <div className="receipt-adder__footer">
+            <BaseButton type="cancel" click={this.onCancelHandler}>Cancelar</BaseButton>
+            <BaseButton type="confirm" click={this.onConfirmHandler}>Confirmar</BaseButton>
+          </div>
+        </section>
+      </Modal>
+    )
+  }
+
+  onConfirmHandler = () => {
+    this.props.history.push('/compare-data-with-receipt')
+  }
+
+  onCancelHandler = () => {
+    console.log("cancel")
+  }
+
 }
 
 export default ReceiptAdder
