d7755aa1ac7d8006a3a7141590c4d18001e1af89
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-09-19 00:23:35 -0300

    Add existing component to its route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReceiptAdder from './components/Receipt/ReceiptAdder/ReceiptAdder'
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
@@ -8,7 +9,7 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route path='/' exact render={() => <h1>Home</h1>}/>    
-          <Route path='/send-receipt' render={() => <h1>send receipt</h1>} />
+          <Route path='/send-receipt' component={ReceiptAdder} />
           <Route path='/compare-data-with-receipt' render={() => <h1>compare data</h1>} />
           <Route path='/edit-receipt-data' render={() => <h1>edit receipt data</h1>} />
           <Route path='/list-all-receipts' render={() => <h1>list all receipts</h1>} />
--- src/components/Receipt/ReceiptAdder/ReceiptAdder.js
+++ src/components/Receipt/ReceiptAdder/ReceiptAdder.js
@@ -2,16 +2,19 @@ import React from 'react'
 import './ReceiptAdder.scss'
 import BaseButton from '../../UI/Button/BaseButton/BaseButton'
 import DropArea from '../../UI/DropArea/DropArea'
+import Modal from '../../UI/Modal/Modal'
 
 const ReceiptAdder = () => {
   return (
-    <section className="receipt-adder">
-      <DropArea />
-      <div className="receipt-adder__footer">
-        <BaseButton type="cancel">Cancelar</BaseButton>
-        <BaseButton type="confirm">Confirmar</BaseButton>
-      </div>
-    </section>
+    <Modal>
+      <section className="receipt-adder">
+        <DropArea />
+        <div className="receipt-adder__footer">
+          <BaseButton type="cancel">Cancelar</BaseButton>
+          <BaseButton type="confirm">Confirmar</BaseButton>
+        </div>
+      </section>
+    </Modal>
   )
 }
 
