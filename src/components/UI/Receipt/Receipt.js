df3fe1087b428eea5b9cb7c9b63771231615b15d
Phe0 <pheofo@gmail.com>
2018-09-19 15:17:29 -0300

    Add Receipt stylesheet and structure
    Co-authored-by: Pedro Féo <pheofo@gmail.com>
    Co-authored-by: youssef-md <emaildeyoussefmuhamad@gmail.com>

--- src/components/UI/Receipt/Receipt.js
+++ src/components/UI/Receipt/Receipt.js
@@ -2,9 +2,10 @@ import React from 'react'
 import './Receipt.scss'
 
 const Receipt = (props) => (
-    <div>
-        <img className="receipt" src="#" />
-        <h1>aaaaaa</h1>
+    <div className="receipt">
+        <div className="receipt__content" >
+            <p className="receipt__content__text">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        </div>
     </div>
 )
 
