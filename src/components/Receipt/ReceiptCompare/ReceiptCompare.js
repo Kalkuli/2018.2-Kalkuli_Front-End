567c4c205e059b32bc165ec706589d6a210a428f
Phe0 <pheofo@gmail.com>
2018-09-20 21:29:51 -0300

    Add json to Receipt in ReceiptCompare

--- src/components/Receipt/ReceiptCompare/ReceiptCompare.js
+++ src/components/Receipt/ReceiptCompare/ReceiptCompare.js
@@ -2,16 +2,53 @@ import React from 'react'
 import './ReceiptCompare.scss'
 import Receipt from '../../UI/Receipt/Receipt'
 import Modal from '../../UI/Modal/Modal'
+import Products from '../../UI/Products/Products'
 
 const ReceiptCompare = () => {
-    return(
-        <Modal>
-            <div className="compare-area">
-                <div className="compare-area__preview">
-                </div>
 
+    var fakeData = [
+        {
+            "receipt": {
+             "company_id": 123,
+             "emission_date": "aaa",
+             "emission_place": "aaaaaa",
+             "tax_value": 20.20,
+             "total_price": 123.12,
+             "products": [{
+               "quantity": 12,
+               "unit_price": 13.3
+              },
+              {
+               "quantity": 12,
+               "unit_price": 13.3
+              }]
+             }
+           }
+    ];
+
+    return(
+
+<Modal>
+            <div className="compare-area">
                 <Receipt size="large">
-                    <h1>aasdadasd</h1>
+                    {fakeData.map((i) => {
+                        return(
+                            <div>
+                                <h1>{i.receipt.company_id}</h1>
+                                <h1>{i.receipt.emission_date}</h1>
+                                <h1>{i.receipt.emission_place}</h1>
+                                <h1>{i.receipt.tax_value}</h1>
+                                <h1>{i.receipt.total_price}</h1>
+
+                                {i.receipt.products.map((j) => {
+                                    return(
+                                        <Products quantity={j.quantity} unit_price={j.unit_price}/>
+                                    )
+                                })}
+                            </div>
+                        )
+                    })}
+
                 </Receipt>
             </div>
         </Modal>
