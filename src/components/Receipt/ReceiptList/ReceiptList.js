a5c6cb97d6a5f62767eea35bedb25ed1325233ce
EsioFreitas <esio.gustavo@gmail.com>
2018-09-21 18:08:38 -0300

    Create Navbar component
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>

--- src/components/Receipt/ReceiptList/ReceiptList.js
+++ src/components/Receipt/ReceiptList/ReceiptList.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import './ReceiptList.scss'
 import Receipt from '../../UI/Receipt/Receipt'
+import Navbar from '../../UI/Navbar/Navbar'
 
 const dados = ['1', '2', '3', '3','4','5','6','7', 7, 8 ,8 , 7 , 7 , 7 , 7, 8]
 const ReceiptList = () => {
     return(
-        
-        <div className='receipt-all-position'>
-            {dados.map( i => {
-                return <Receipt size="small"/>
-            })}
+        <div>
+            < Navbar />
+            <div className='receipt-all-position'>
+                {dados.map( i => {
+                    return <Receipt size="small"/>
+                })}
             </div>
+        </div>
     )
 }
 
---
+++ src/components/UI/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.scss'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar-logo">
+                <nav><a href="/"><h1>Kalkuli</h1></a></nav>
+            </div>
+            <lu className="navbar-menu">                
+                <nav><a href="/">Empresa</a></nav>
+                <nav><a href="/">Dados</a></nav>
+                <nav><a href="/">Notas</a></nav>
+                <nav><a href="/">Sair</a></nav>
+            </lu>
+        </div>
+    )
+}
+   
+export default Navbar
