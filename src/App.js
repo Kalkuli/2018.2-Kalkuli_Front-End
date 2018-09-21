5afc3a0ea7e28d3408fb3af26d44243da327282e
EsioFreitas <esio.gustavo@gmail.com>
2018-09-21 18:40:34 -0300

    Add font style navbar
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import ReceiptAdder from './components/Receipt/ReceiptAdder/ReceiptAdder'
 import ReceiptCompare from './components/Receipt/ReceiptCompare/ReceiptCompare'
-<<<<<<< cc565a17c80f426958a5cbdca02bf625dacfa620
 import Confirmation from './components/Confirmation/Confirmation'
-=======
 import ReceiptList from './components/Receipt/ReceiptList/ReceiptList'
->>>>>>> Order all receipts
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
@@ -18,12 +15,8 @@ class App extends Component {
           <Route path='/send-receipt' component={ReceiptAdder} />
           <Route path='/compare-data-with-receipt' component={ReceiptCompare}/>
           <Route path='/edit-receipt-data' render={() => <h1>edit receipt data</h1>} />
-<<<<<<< cc565a17c80f426958a5cbdca02bf625dacfa620
-          <Route path='/list-all-receipts' render={() => <h1>list all receipts</h1>} />
-          <Route path='/confirmation' component={Confirmation} />
-=======
           <Route path='/list-all-receipts'component={ReceiptList} />
->>>>>>> Order all receipts
+          <Route path='/confirmation' component={Confirmation} />
           <Route render={() => <h1>Not found</h1>} />
         </Switch>
       </BrowserRouter>
--- src/components/UI/Navbar/Navbar.js
+++ src/components/UI/Navbar/Navbar.js
@@ -8,10 +8,10 @@ const Navbar = () => {
                 <nav><a href="/"><h1>Kalkuli</h1></a></nav>
             </div>
             <lu className="navbar-menu">                
-                <nav><a href="/">Empresa</a></nav>
-                <nav><a href="/">Dados</a></nav>
-                <nav><a href="/">Notas</a></nav>
-                <nav><a href="/">Sair</a></nav>
+                <nav><a href="#">Empresa</a></nav>
+                <nav><a href="#">Dados</a></nav>
+                <nav><a href="/list-all-receipts">Notas</a></nav>
+                <nav><a href="#">Sair</a></nav>
             </lu>
         </div>
     )
