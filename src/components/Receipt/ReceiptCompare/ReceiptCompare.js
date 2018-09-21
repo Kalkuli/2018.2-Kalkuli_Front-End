bcd7b6b04a69c4196ace1d8de7f915608720af85
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-09-21 10:29:41 -0300

    Map custom Input for editting receipt data

--- src/components/Receipt/ReceiptCompare/ReceiptCompare.js
+++ src/components/Receipt/ReceiptCompare/ReceiptCompare.js
@@ -1,52 +1,55 @@
-import React from 'react'
+import React,{ Component } from 'react'
 import './ReceiptCompare.scss'
 import Receipt from '../../UI/Receipt/Receipt'
 import Modal from '../../UI/Modal/Modal'
-import Products from '../../UI/Products/Products'
+import Input from '../../UI/Input/Input'
 
-const ReceiptCompare = () => {
+class ReceiptCompare extends Component {
 
-    var fakeData = {
-        "receipt": {
-            "company_id": 123,
-            "emission_date": "aaa",
-            "emission_place": "aaaaaa",
-            "tax_value": 20.20,
-            "total_price": 123.12,
-            "products": [{
-                "id": 123,
-                "quantity": 12,
-                "unit_price": 13.3
-            },
-            {
-                "id": 1233123,
-                "quantity": 12,
-                "unit_price": 13.3
-            }]
+    render() {
+
+        var fakeData = {
+            "receipt": {
+                "company_id": 123,
+                "emission_date": "aaa",
+                "emission_place": "aaaaaa",
+                "tax_value": 20.20,
+                "total_price": 123.12,
+                "products": [{
+                    "id": 123,
+                    "quantity": 12,
+                    "unit_price": 13.3
+                },
+                {
+                    "id": 1233123,
+                    "quantity": 12,
+                    "unit_price": 1.22
+                }]
+            }
         }
+    
+        console.log(fakeData.receipt)
+    
+        return(
+            <Modal>
+                <div className="compare-area">
+                    <Receipt size="large">
+                        <div className="compare-area__content">
+                            <Input placeholder={fakeData.receipt.company_id} />
+                            <Input placeholder={fakeData.receipt.emission_date} />
+                            <Input placeholder={fakeData.receipt.emission_place} />
+                            <Input placeholder={fakeData.receipt.tax_value} />
+                            <Input placeholder={fakeData.receipt.total_price} />
+    
+                            {fakeData.receipt.products.map(product => 
+                                <Input key={product.id} placeholder={product.unit_price} />
+                            )}
+                        </div>
+                    </Receipt>
+                </div>
+            </Modal>
+        )
     }
-
-    console.log(fakeData.receipt)
-
-    return(
-        <Modal>
-            <div className="compare-area">
-                <Receipt className="receipt-font" size="large">
-                    <div>
-                        <h1 className="receipt-font">{fakeData.receipt.company_id}</h1>
-                        <h1 className="receipt-font">{fakeData.receipt.emission_date}</h1>
-                        <h1 className="receipt-font">{fakeData.receipt.emission_place}</h1>
-                        <h1 className="receipt-font">{fakeData.receipt.tax_value}</h1>
-                        <h1 className="receipt-font">{fakeData.receipt.total_price}</h1>
-
-                        {fakeData.receipt.products.map(product => (
-                            <Products key={product.id} quantity={product.quantity} unit_price={product.unit_price}/>
-                        ))}
-                    </div>
-                </Receipt>
-            </div>
-        </Modal>
-    )
 }
 
 export default ReceiptCompare
