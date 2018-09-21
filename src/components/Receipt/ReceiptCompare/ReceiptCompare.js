54a4a67d97f2b2f22a40a4e1b63794e87d70ecef
Phe0 <pheofo@gmail.com>
2018-09-20 21:29:51 -0300

    Add json to Receipt in ReceiptCompare

--- src/components/Receipt/ReceiptCompare/ReceiptCompare.js
+++ src/components/Receipt/ReceiptCompare/ReceiptCompare.js
@@ -2,6 +2,7 @@ import React,{ Component } from 'react'
 import './ReceiptCompare.scss'
 import Receipt from '../../UI/Receipt/Receipt'
 import Modal from '../../UI/Modal/Modal'
+<<<<<<< d5c60df6ec2208651fd10a5bcc51084edded6bd2
 import Input from '../../UI/Input/Input'
 
 class ReceiptCompare extends Component {
@@ -62,6 +63,59 @@ class ReceiptCompare extends Component {
 		console.log(oldData.receipt[""+inputID+""])
 		this.setState({fakeData: oldData})
 	}
+=======
+import Products from '../../UI/Products/Products'
+
+const ReceiptCompare = () => {
+
+    var fakeData = [
+        {
+            "receipt": {
+             "company_id": 123,
+             "emission_date": "aaa",
+             "emission_place": "aaaaaa",
+             "tax_value": 20.20,
+             "total_price": 123.12,
+             "products": [{
+               "quantity": 12,
+               "unit_price": 13.3
+              },
+              {
+               "quantity": 12,
+               "unit_price": 13.3
+              }]
+             }
+           }
+    ];
+
+    return(
+
+<Modal>
+            <div className="compare-area">
+                <Receipt size="large">
+                    {fakeData.map((i) => {
+                        return(
+                            <div>
+                                <h1>{i.receipt.company_id}</h1>
+                                <h1>{i.receipt.emission_date}</h1>
+                                <h1>{i.receipt.emission_place}</h1>
+                                <h1>{i.receipt.tax_value}</h1>
+                                <h1>{i.receipt.total_price}</h1>
+
+                                {i.receipt.products.map((j) => {
+                                    return(
+                                        <Products quantity={j.quantity} unit_price={j.unit_price}/>
+                                    )
+                                })}
+                            </div>
+                        )
+                    })}
+
+                </Receipt>
+            </div>
+        </Modal>
+    )
+>>>>>>> Add json to Receipt in ReceiptCompare
 }
 
 export default ReceiptCompare
