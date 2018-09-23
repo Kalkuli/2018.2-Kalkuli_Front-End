b6342353ea45b8215dd03cfe5dc2cbc1d48e7254
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-09-22 21:53:02 -0300

    Return pdf in base64 and improve scss

--- src/components/UI/DropArea/DropArea.js
+++ src/components/UI/DropArea/DropArea.js
@@ -4,29 +4,49 @@ import ReactDropzone from 'react-dropzone'
 
 class DropArea extends Component {
 
+  state = {
+    preview: null
+  }
+
   render() {
     return (
-      <ReactDropzone className="drop-area" 
-                     onDrop={this.onDropHandler}
-                     multiple={false}
-                     accept="application/pdf"
-                     acceptClassName="accept">
-        <h1 className="drop-area__title">Arraste o arquivo</h1>
-        <p className="drop-area__p">ou</p>
-        <p className="drop-area__p">clique dentro desta área</p>
-      </ReactDropzone>
+      <div>
+        <ReactDropzone className="drop-area" 
+                      onDrop={this.onDropHandler}
+                      accept="application/pdf"
+                      multiple={false}
+                      acceptClassName="accept">
+          <h1 className="drop-area__title">Arraste o arquivo</h1>
+          <p className="drop-area__p">ou</p>
+          <p className="drop-area__p">clique dentro desta área</p>
+        </ReactDropzone>
+      </div>
+
     )
   }
 
   onDropHandler = (file, rejectedFiles) => {    
+
     if(file.length === 1){
-      alert("preview do arquivo: " + file[0].preview)
+      //alert("preview do arquivo: " + file[0].preview)
       console.log("Arquivo aceito:", file)
+
+      const currentFile = file[0]
+      const reader = new FileReader()
+      reader.addEventListener("load", () =>   {
+        this.setState({preview: reader.result})
+        console.log(this.state.preview)
+      }, false)
+      reader.readAsDataURL(currentFile)
+
     } else if(rejectedFiles) {
       alert("Só aceitamos 1 arquivo PDF")
       console.log("arquivo rejeitado: ", rejectedFiles)
     }
+
   }
+
+ 
 
 }
 
