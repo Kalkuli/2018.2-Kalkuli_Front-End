7ec31e2f0fbda7af010d890ca86ce266cccf59ef
EsioFreitas <esio.gustavo@gmail.com>
2018-09-27 18:59:06 -0300

    Add data in receiptView
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/components/Receipt/ReceiptView/ReceiptView.js
+++ src/components/Receipt/ReceiptView/ReceiptView.js
@@ -5,10 +5,23 @@ import Receipt from '../../UI/Receipt/Receipt'
 import BaseButton from '../../UI/Button/BaseButton/BaseButton'
 
 class ReceiptView extends Component{
+    receipt = {
+        "company_id": "00.000.000/0000-00",
+        "emission_date": "00/00/0000",
+        "emission_place": "Gama",
+        "tax_value": 20.20
+    }
     render(){
         return(
             < Modal>
                 < Receipt size='large' className="receipt">
+                    <div className='receipt-area receipt-font'>
+                        <p>{this.receipt.company_id}</p>
+                        <p>{this.receipt.emission_date}</p>
+                        <p>{this.receipt.emission_place}</p>
+                        <p>{this.receipt.tax_value}</p>
+
+                    </div>
                 </Receipt>
                 <div className='area-buttons'>
                     <div className='change-buttons'>
