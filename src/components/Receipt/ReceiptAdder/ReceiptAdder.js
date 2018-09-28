9b1acb2177250d2b36702f7f4a54c4a8b351b641
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-09-28 19:17:25 -0300

    Remove state from DropArea and move onDropHandler to a higher component
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/components/Receipt/ReceiptAdder/ReceiptAdder.js
+++ src/components/Receipt/ReceiptAdder/ReceiptAdder.js
@@ -6,21 +6,43 @@ import Modal from '../../UI/Modal/Modal'
 
 class ReceiptAdder extends Component {
 
+  state = {
+    preview: null
+  }
   render() {
 
     return (
       <Modal>
         <section className="receipt-adder">
-          <DropArea />
+          <DropArea onDropHandler={this.onDropHandler}/>
           <div className="receipt-adder__footer">
             <BaseButton type="edit" click={this.onCancelHandler}>Cancelar</BaseButton>
             <BaseButton type="confirm" click={this.onConfirmHandler}>Confirmar</BaseButton>
+            <embed src={this.state.preview} type="application/pdf" width="300px" height="800px" />
           </div>
         </section>
       </Modal>
     )
   }
 
+  onDropHandler = (file, rejectedFiles) => {  
+    if(file.length === 1){
+      //alert("preview do arquivo: " + file[0].preview)
+      console.log("Arquivo aceito:", file)
+
+      const currentFile = file[0]
+      const reader = new FileReader()
+      reader.addEventListener("load", () =>   {
+        this.setState({preview: reader.result})
+        console.log(this.state.preview)
+      }, false)
+      reader.readAsDataURL(currentFile)
+
+    } else if(rejectedFiles) {
+      alert("Só aceitamos 1 arquivo PDF")
+      console.log("arquivo rejeitado: ", rejectedFiles)
+    }
+  }
   onConfirmHandler = () => {
     this.props.history.push('/compare-data-with-receipt')
   }
