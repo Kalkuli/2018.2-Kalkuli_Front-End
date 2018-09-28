a30f928e5125891cf3caaf562861569a8ad9bd77
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-09-28 13:09:53 -0300

    Improve receipt list styling
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/components/Receipt/ReceiptList/List.js
+++ src/components/Receipt/ReceiptList/List.js
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import './ReceiptList.scss'
 import Receipt from '../../UI/Receipt/Receipt'
@@ -23,27 +22,35 @@ const dadosNota = [
     "emission_date": "00/00/0000",
     "emission_place": "Gama",
     "tax_value": 20.20
+	},
+	{
+    "company_id": "00.000.000/0000-00",
+    "emission_date": "00/00/0000",
+    "emission_place": "Gama",
+    "tax_value": 20.20
+	},
+	{
+    "company_id": "00.000.000/0000-00",
+    "emission_date": "00/00/0000",
+    "emission_place": "Gama",
+    "tax_value": 20.20
 	}]
-class ReceiptList extends React.Component {
 
+class ReceiptList extends React.Component {
 
 	render(){
 			return(
-				<div className='receipt-all-position'>
+				<div className='container-receipts'>
 					{dadosNota.map( dados => (
-						<div className="receipt-distance">
-							<Receipt size="small" onClickHandler={this.onClosePopup}>
-								<div className='receipt-font'>
-									<div className='dadosNotas'>
-										<p className='dados'>{dados.company_id}</p>
-										<p className='dados'>{dados.emission_date}</p>
-										<p className='dados'>{dados.emission_place}</p>
-										<p className='dados'>{dados.tax_value}</p>
-									</div>
-								</div>
-							</Receipt>
+						<Receipt size="small" onClickHandler={this.onClosePopup}>
+							<div className='container-receipts__receipt-data receipt-font'>
+								<p>{dados.company_id}</p>
+								<p>{dados.emission_date}</p>
+								<p>{dados.emission_place}</p>
+								<p>{dados.tax_value}</p>
+							</div>
 							{/* <ReceiptView onClosePopup={this.onClosePopup}/> */}
-						</div>
+						</Receipt>
 					))}
 				</div>
 			)
--- src/components/Receipt/ReceiptList/ReceiptList.js
+++ src/components/Receipt/ReceiptList/ReceiptList.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
+<<<<<<< 58c65bc65036e8c51d35e5eb22fe43e9dc0f0155
 //import Axios from 'axios'
 
+=======
+>>>>>>> Improve receipt list styling
 import List from './List'
 import Navbar from '../../UI/Navbar/Navbar'
 import './ReceiptList.scss'
