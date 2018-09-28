0ae24aadeffc17ef4fcd8e1d43918c0289573f4c
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-09-28 20:29:07 -0300

    Connect ReceiptAdder to Redux to dispatch an action containing the fileBLOB
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/components/Receipt/ReceiptAdder/ReceiptAdder.js
+++ src/components/Receipt/ReceiptAdder/ReceiptAdder.js
@@ -3,12 +3,15 @@ import './ReceiptAdder.scss'
 import BaseButton from '../../UI/Button/BaseButton/BaseButton'
 import DropArea from '../../UI/DropArea/DropArea'
 import Modal from '../../UI/Modal/Modal'
+import { connect } from 'react-redux'
+import * as actionTypes from '../../../store/actions/actions'
 
 class ReceiptAdder extends Component {
 
   state = {
     preview: null
   }
+
   render() {
 
     return (
@@ -27,30 +30,35 @@ class ReceiptAdder extends Component {
 
   onDropHandler = (file, rejectedFiles) => {  
     if(file.length === 1){
-      //alert("preview do arquivo: " + file[0].preview)
-      console.log("Arquivo aceito:", file)
-
       const currentFile = file[0]
       const reader = new FileReader()
       reader.addEventListener("load", () =>   {
         this.setState({preview: reader.result})
-        console.log(this.state.preview)
+        this.props.onFileAdded(this.state.preview)
       }, false)
       reader.readAsDataURL(currentFile)
-
     } else if(rejectedFiles) {
       alert("Só aceitamos 1 arquivo PDF")
       console.log("arquivo rejeitado: ", rejectedFiles)
     }
   }
-  onConfirmHandler = () => {
-    this.props.history.push('/compare-data-with-receipt')
-  }
 
-  onCancelHandler = () => {
-    console.log("cancel")
-  }
+  onConfirmHandler = () => { this.props.history.push('/compare-data-with-receipt') }
+
+  onCancelHandler = () => { console.log("cancel") }
 
 }
 
-export default ReceiptAdder
+const mapStateToProps = state => {
+  return {
+    file: state.fileBLOB
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onFileAdded: (file) => dispatch({ type: actionTypes.ADD_FILE, file: file})
+  }
+}
+
+export default connect(null, mapDispatchToProps)(ReceiptAdder)
--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -7,6 +7,7 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch(action.type) {
     case actionTypes.ADD_FILE:
+      console.log(action.file)
       return {
         ...state,
         fileBLOB: action.file
