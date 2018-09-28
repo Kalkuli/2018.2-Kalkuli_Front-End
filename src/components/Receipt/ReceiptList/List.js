59eed5ad672f739f04da62e8583c28c4ca1834f5
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-09-28 13:09:53 -0300

    Improve receipt list styling
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/components/Receipt/ReceiptList/List.js
+++ src/components/Receipt/ReceiptList/List.js
@@ -1,7 +1,10 @@
+<<<<<<< e1623afceabe202c57c8f7f0da08d564a908413c
 <<<<<<< 825325c8f09e218d824aab8c4de058b3f08e8909
 =======
 
 >>>>>>> Improve scss and event handling
+=======
+>>>>>>> Improve receipt list styling
 import React from 'react'
 import './ReceiptList.scss'
 import Receipt from '../../UI/Receipt/Receipt'
@@ -26,7 +29,10 @@ const dadosNota = [
     "emission_date": "00/00/0000",
     "emission_place": "Gama",
     "tax_value": 20.20
+<<<<<<< e1623afceabe202c57c8f7f0da08d564a908413c
 <<<<<<< 825325c8f09e218d824aab8c4de058b3f08e8909
+=======
+>>>>>>> Improve receipt list styling
 	},
 	{
     "company_id": "00.000.000/0000-00",
@@ -39,6 +45,7 @@ const dadosNota = [
     "emission_date": "00/00/0000",
     "emission_place": "Gama",
     "tax_value": 20.20
+<<<<<<< e1623afceabe202c57c8f7f0da08d564a908413c
 	}]
 
 class ReceiptList extends React.Component {
@@ -57,28 +64,30 @@ class ReceiptList extends React.Component {
 							{/* <ReceiptView onClosePopup={this.onClosePopup}/> */}
 						</Receipt>
 =======
+=======
+>>>>>>> Improve receipt list styling
 	}]
-class ReceiptList extends React.Component {
 
+class ReceiptList extends React.Component {
 
 	render(){
 			return(
-				<div className='receipt-all-position'>
+				<div className='container-receipts'>
 					{dadosNota.map( dados => (
-						<div className="receipt-distance">
-							<Receipt size="small" onClickHandler={this.onClosePopup}>
-								<div className='receipt-font'>
-									<div className='dadosNotas'>
-										<p className='dados'>{dados.company_id}</p>
-										<p className='dados'>{dados.emission_date}</p>
-										<p className='dados'>{dados.emission_place}</p>
-										<p className='dados'>{dados.tax_value}</p>
-									</div>
-								</div>
-							</Receipt>
+						<Receipt size="small" onClickHandler={this.onClosePopup}>
+							<div className='container-receipts__receipt-data receipt-font'>
+								<p>{dados.company_id}</p>
+								<p>{dados.emission_date}</p>
+								<p>{dados.emission_place}</p>
+								<p>{dados.tax_value}</p>
+							</div>
 							{/* <ReceiptView onClosePopup={this.onClosePopup}/> */}
+<<<<<<< e1623afceabe202c57c8f7f0da08d564a908413c
 						</div>
 >>>>>>> Improve scss and event handling
+=======
+						</Receipt>
+>>>>>>> Improve receipt list styling
 					))}
 				</div>
 			)
