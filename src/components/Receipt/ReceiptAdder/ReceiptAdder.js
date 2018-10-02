0abd70d19b1136b17b35766d3c9185649d70d9d4
Dutra <ldutra98@gmail.com>
2018-10-02 00:02:45 -0300

    Create label to Compare Receipts
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Pedro Féo <pheofo@gmail.com>
    Co-authored-by: Saleh Nazih <saleh.nazih.dev@gmail.com>

--- src/components/Receipt/ReceiptAdder/ReceiptAdder.js
+++ src/components/Receipt/ReceiptAdder/ReceiptAdder.js
@@ -81,7 +81,7 @@ class ReceiptAdder extends Component {
     })
     let formData = new FormData();
     formData.append("file", this.state.file[0]);
-    axios.post('http://172.31.0.1:5008/api/v1/extract_data', formData, {
+    axios.post('http://172.23.0.1:5008/api/v1/extract_data', formData, {
       headers: {
         'Content-Type': 'multipart/form-data'
       }
