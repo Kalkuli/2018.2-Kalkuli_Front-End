810c46b35cb9871c6fdff12dcee920b24e369187
EsioFreitas <esio.gustavo@gmail.com>
2018-10-03 17:39:26 -0300

    Add de button description

--- src/components/Receipt/ReceiptList/ReceiptList.js
+++ src/components/Receipt/ReceiptList/ReceiptList.js
@@ -38,6 +38,9 @@ export default class ReceiptList extends Component {
     }
     changeRotate(){ 
         this.setState({rotated: !this.state.rotated, display: !this.state.display});
+        if(this.state.rotated === true){
+            
+        }
     }
 
     render() {
@@ -50,17 +53,26 @@ export default class ReceiptList extends Component {
                     style={{transform: this.state.rotated ? 'rotate(45deg)' : 'rotate(0)'}} />
                 </div>
                 <div className='receipt__options'>
-                    <div className='receipt__options--newReceipt scalehover'
-                    style={{display: this.state.display ? 'none' : 'block'}}>
-                        <div className='receipt__img__container'>
-                            <div className='receipt__options__imgReceipt'/>
+                    <div style={{display: this.state.display ? 'none' : 'block'}}>
+                        <div className='receipt__options--newReceipt scalehover'>
+                            <div className='receipt__img__container'>
+                                <div className='receipt__options__imgReceipt'/>
+                            </div>
                         </div>
                     </div>
-
-                    <div className='receipt__options--newReport scalehover'
-                    style={{display: this.state.display ? 'none' : 'block'}}>
-                        <div className='receipt__img__container'>
-                            <div className='receipt__options__imgReport'/>  
+                    <div style={{display: this.state.display ? 'none' : 'block'}}>
+                        <div className='receipt__options--newReport scalehover'>
+                            <div className='receipt__img__container'>
+                                <div className='receipt__options__imgReport'/>  
+                            </div>
+                        </div>
+                    </div>
+                    <div className='receipt__report__names'>
+                        <div style={{display: this.state.display ? 'none' : 'block'}}>
+                            <p>Nova Nota</p>
+                        </div>
+                        <div style={{display: this.state.display ? 'none' : 'block'}}>
+                            <p className='receipt__report__name--text'>Novo relatório</p>
                         </div>
                     </div>
                 </div>
