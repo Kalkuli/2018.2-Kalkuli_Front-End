1a1bacd1dad1c5f4f464c6cfa27a14ffaf8d066a
Phe0 <pheofo@gmail.com>
2018-10-03 08:36:28 -0300

    Disable drop and file selection after selecting one file

--- src/components/UI/DropArea/DropArea.js
+++ src/components/UI/DropArea/DropArea.js
@@ -5,15 +5,22 @@ import Receipt from '../../UI/Receipt/Receipt'
 
 const DropArea = (props) => (
   <Fragment>
-    {props.fileSelected ? <Receipt size="large">Arquivo recebido com sucesso!</Receipt> : <ReactDropzone className="drop-area"
-                                                      onDrop={props.onDropHandler}
-                                                      accept="application/pdf"
-                                                      multiple={false}
-                                                      acceptClassName="accept">
-                                                      <h1 className="drop-area__title">Arraste o arquivo</h1>
-                                                      <p className="drop-area__p">ou</p>
-                                                      <p className="drop-area__p">clique dentro desta área</p>
-                                          </ReactDropzone>}
+    {props.fileSelected ? <ReactDropzone className="drop-area"
+                                      onDrop={props.onDropHandler}
+                                      accept="application/pdf"
+                                      multiple={false}
+                                      acceptClassName="accept"
+                                      disabled="true">
+                                      <h1 className="drop-area__title">Arquivo Selecionado com Sucesso</h1>
+                          </ReactDropzone> : <ReactDropzone className="drop-area"
+                                                        onDrop={props.onDropHandler}
+                                                        accept="application/pdf"
+                                                        multiple={false}
+                                                        acceptClassName="accept">
+                                                        <h1 className="drop-area__title">Arraste o arquivo</h1>
+                                                        <p className="drop-area__p">ou</p>
+                                                        <p className="drop-area__p">clique dentro desta área</p>
+                                            </ReactDropzone>}
   </Fragment>
 )
 
