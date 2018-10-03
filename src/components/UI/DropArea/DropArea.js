2a59e66b78d15e0a86610bb611e750f20181e71f
Dutra <ldutra98@gmail.com>
2018-10-03 01:09:18 -0300

    Add conditional to verify if the system received a file
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Pedro Féo <pheofo@gmail.com>
    Co-authored-by: Saleh Nazih <saleh.nazih.dev@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/components/UI/DropArea/DropArea.js
+++ src/components/UI/DropArea/DropArea.js
@@ -1,18 +1,19 @@
 import React, { Fragment } from 'react'
 import "./DropArea.scss"
 import ReactDropzone from 'react-dropzone'
+import Receipt from '../../UI/Receipt/Receipt'
 
 const DropArea = (props) => (
   <Fragment>
-    <ReactDropzone className="drop-area"
-      onDrop={props.onDropHandler}
-      accept="application/pdf"
-      multiple={false}
-      acceptClassName="accept">
-      <h1 className="drop-area__title">Arraste o arquivo</h1>
-      <p className="drop-area__p">ou</p>
-      <p className="drop-area__p">clique dentro desta área</p>
-    </ReactDropzone>
+    {props.fileSelected ? <Receipt size="large">Arquivo recebido com sucesso!</Receipt> : <ReactDropzone className="drop-area"
+                                                      onDrop={props.onDropHandler}
+                                                      accept="application/pdf"
+                                                      multiple={false}
+                                                      acceptClassName="accept">
+                                                      <h1 className="drop-area__title">Arraste o arquivo</h1>
+                                                      <p className="drop-area__p">ou</p>
+                                                      <p className="drop-area__p">clique dentro desta área</p>
+                                          </ReactDropzone>}
   </Fragment>
 )
 
