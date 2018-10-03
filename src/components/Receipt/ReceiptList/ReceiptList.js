588772c907993f4a7dbd57320e20b0b47254e53d
EsioFreitas <esio.gustavo@gmail.com>
2018-10-03 07:43:11 -0300

    Add the rotate state to move the plus

--- src/components/Receipt/ReceiptList/ReceiptList.js
+++ src/components/Receipt/ReceiptList/ReceiptList.js
@@ -12,9 +12,12 @@ import './ReceiptList.scss'
 export default class ReceiptList extends Component {
     constructor(props) {
         super(props)
-        this.state = {
+        this.state = {  
             loaded: false,
+            rotated: false 
         }
+
+        this.changeRotate = this.changeRotate.bind(this);
     }
 
     componentDidMount() {
@@ -32,14 +35,18 @@ export default class ReceiptList extends Component {
                 console.log(this.state);
             })
     }
+    changeRotate(){ 
+        this.setState({rotated: !this.state.rotated});
+
+    }
 
     render() {
         return (
             <div >
                 <Navbar />
                 {this.state.loaded && <List receipts={this.state.receipts} />}
-                <div className='receipt'>
-                    <div className='receipt__plus'></div>
+                <div className='receipt' onClick={this.changeRotate} >
+                    <div className='receipt__plus' style={{transform: this.state.rotated ? 'rotate(45deg)' : 'rotate(0)'}} />
                 </div>
             </div>
         )
