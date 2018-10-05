626d772c9ab2720626020e24f3aa027a6f63312d
Phe0 <pheofo@gmail.com>
2018-10-05 20:57:28 -0300

    Style datepicker

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -52,21 +52,25 @@ class Dashboard extends Component{
     render(){
         return(
             <div className="dashboard">
-            <DateRangePicker
-            startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-            startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-            endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-            endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-            onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
-            focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-            onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-            displayFormat="D/M/Y"
-            />
-            {console.log(this.state.startDate)}
+                {console.log(this.state.startDate)}
                 <Navbar/>
-                <div className="dashboard__report-area">
-                    <Report data={info.fakeData.report.receipts}/>
-                    <BaseButton size="small" type="confirm" click={this.onConfirmHandler}>Salvar Relatório</BaseButton>
+                <div className="dashboard__content">
+                    <div className="dashboard__content__datepicker">
+                        <DateRangePicker
+                        startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+                        startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+                        endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+                        endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+                        onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+                        focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                        onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+                        displayFormat="D/M/Y"
+                        />
+                    </div>
+                    <div className="dashboard__content__report-area">
+                        <Report data={info.fakeData.report.receipts}/>
+                        <BaseButton size="small" type="confirm" click={this.onConfirmHandler}>Salvar Relatório</BaseButton>
+                    </div>
                 </div>
             </div>
         )
