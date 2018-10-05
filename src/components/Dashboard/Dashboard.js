e32c425ee097426ac2d3b3565a10ee169fc7d357
Phe0 <pheofo@gmail.com>
2018-10-05 15:01:48 -0300

    Add Date Picker to Dashboard
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Pedro Féo <pheofo@gmail.com>
    Co-authored-by: Saleh Nazih <saleh.nazih.dev@gmail.com>

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,6 +4,10 @@ import Navbar from '../UI/Navbar/Navbar'
 import Report from '../UI/Report/Report';
 import BaseButton from '../UI/Button/BaseButton/BaseButton'
 
+import 'react-dates/initialize';
+import {DateRangePicker} from 'react-dates';
+import './DatePicker.scss'
+
 const info = {
     fakeData: {
         "report": {
@@ -35,9 +39,30 @@ const info = {
 }
 
 class Dashboard extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            startDate: null,
+            endDate: null,
+            focusedInput: null
+        }
+    }
+
     render(){
         return(
             <div className="dashboard">
+            <DateRangePicker
+            startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+            startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+            endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+            endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+            onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+            focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+            onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+            displayFormat="D/M/Y"
+            />
+            {console.log(this.state.startDate)}
                 <Navbar/>
                 <div className="dashboard__report-area">
                     <Report data={info.fakeData.report.receipts}/>
