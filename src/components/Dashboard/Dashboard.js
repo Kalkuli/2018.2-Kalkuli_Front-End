f84c84f5dcd063d5616a0debadfb9f136d0c8fa6
Phe0 <pheofo@gmail.com>
2018-10-08 11:06:03 -0300

    Fixing Style of date picker and delete fake data from Reports
    Co-authored-by: Pedro Féo <pheofo@gmail.com>
    Co-authored-by: Ésio Freitas <esio.gustavo@gmail.com>

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -70,6 +70,7 @@ class Dashboard extends Component{
                         onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
                         focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
                         onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+                        isOutsideRange={() => false}
                         />
                     </div>
                     <div className="dashboard__content__report-area">
