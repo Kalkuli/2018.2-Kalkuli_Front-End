f4d7d1446c35d45f726c574ba4cf916c6ec16eb6
EsioFreitas <esio.gustavo@gmail.com>
2018-10-08 12:16:55 -0300

    Add datepicker in report
    Co-authored-by: Pedro Feo <pheofo@gmail.com>
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>

--- src/components/Reports/Reports.js
+++ src/components/Reports/Reports.js
@@ -3,10 +3,165 @@ import './Reports.scss'
 import BaseButton from '../UI/Button/BaseButton/BaseButton'
 import Navbar from '../UI/Navbar/Navbar'
 import Report from '../UI/Report/Report'
+import {DateRangePicker} from 'react-dates';
 
 const info = {
     fakeData: {
         "report": [{
+            "period": "01/09/2018 - 30/09/2018",
+            "receipts":[{
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'zz.zzz.zzz/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            }]
+        },{
+            "period": "01/09/2018 - 30/09/2018",
+            "receipts":[{
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'zz.zzz.zzz/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            }]
+        }, {
+            "period": "01/09/2018 - 30/09/2018",
+            "receipts":[{
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'zz.zzz.zzz/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            }]
+        }, {
+            "period": "01/09/2018 - 30/09/2018",
+            "receipts":[{
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'zz.zzz.zzz/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            }]
+        }, {
+            "period": "01/09/2018 - 30/09/2018",
+            "receipts":[{
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'zz.zzz.zzz/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            }]
+        }, {
+            "period": "01/09/2018 - 30/09/2018",
+            "receipts":[{
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'zz.zzz.zzz/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            }]
+        }, {
+            "period": "01/09/2018 - 30/09/2018",
+            "receipts":[{
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'zz.zzz.zzz/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            },
+            {
+                date: '27/09/2018',
+                cnpj: 'xx.xxx.xxx/xxxx-xx',
+                price: '20,48'  
+            }]
+        }, {
             "period": "01/09/2018 - 30/09/2018",
             "receipts":[{
                 date: '27/09/2018',
@@ -48,19 +203,35 @@ class Reports extends Component {
             <div className="reports">
                 <Navbar/>
                 <div className="reports__area">
-                    <div className="reports__area__resumes">
-                        {info.fakeData.report.map((data, index) => {
-                            if(this.state.position === index){
-                                type = "confirm";
-                            }
-                            else{
-                                type = "cancel";
-                            }
-                            return(
-                                <BaseButton size="medium" type={type} click={() => {this.onConfirmHandler(index)}} >{data.period}</BaseButton>
-                            )
-                        })}
-                    </div>
+                    <di>
+                        <div className="dashboard__content__datepicker">
+                            <DateRangePicker
+                            startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+                            startDatePlaceholderText="Data Inicial"
+                            endDatePlaceholderText="Data Final"
+                            startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+                            endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+                            endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+                            onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+                            focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                            onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+                            isOutsideRange={() => false}
+                            />
+                        </div>
+                        <div className="reports__area__resumes">
+                            {info.fakeData.report.map((data, index) => {
+                                if(this.state.position === index){
+                                    type = "confirm";
+                                }
+                                else{
+                                    type = "cancel";
+                                }
+                                return(
+                                    <BaseButton size="medium" type={type} click={() => {this.onConfirmHandler(index)}} >{data.period}</BaseButton>
+                                )
+                            })}
+                        </div>
+                    </di>
                     <Report data={info.fakeData.report[this.state.position].receipts}/>
                 </div>
                 <div className="reports__button">
