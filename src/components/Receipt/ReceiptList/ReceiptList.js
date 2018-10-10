21828d8d6f4934a812eaf3de36bf3977a7ac6771
devsalula <saleh.nazih.dev@gmail.com>
2018-10-10 14:49:33 -0300

    Join modal in the notes page
    Co-authored-by: Youssef <emaildeyoussefmuhamad@gmail.com>
    Co-authored-by: Saleh <saleh.nazih.dev@gmail.com>

--- src/components/Receipt/ReceiptList/ReceiptList.js
+++ src/components/Receipt/ReceiptList/ReceiptList.js
@@ -4,13 +4,16 @@ import List from './List'
 import Navbar from '../../UI/Navbar/Navbar'
 import './ReceiptList.scss'
 import MenuButton from '../../UI/Button/MenuButton/MenuButton'
+import ReceiptAdder from '../../Receipt/ReceiptAdder/ReceiptAdder'
+import Backdrop from '../../UI/BackDrop/BackDrop'
 
 export default class ReceiptList extends Component {
 
     state = {  
         loaded: false,
         display: 'block',
-        rotate: false
+        rotate: false,
+        newReceipt: false
     }
 
 
@@ -32,11 +35,21 @@ export default class ReceiptList extends Component {
 
     changeRotate = () => { 
         this.setState({rotate: !this.state.rotate, display: !this.state.display});
-        console.log('rodando rodando!')
+    }
+
+    renderModal = () => {
+        return <div>
+                <Backdrop show={this.state.newReceipt} click={this.onCloseReceiptAdder} />
+                <ReceiptAdder />
+               </div>
+    } 
+
+    onCloseReceiptAdder = () => {
+        this.setState({newReceipt: false})
     }
 
     onNewReceiptHandler = () => {
-        console.log('nova nota')
+        this.setState({newReceipt: true})
     }
 
     onNewReportHandler = () => {
@@ -48,6 +61,7 @@ export default class ReceiptList extends Component {
             <div >
                 <Navbar />
                 {this.state.loaded && <List receipts={this.state.receipts} />}
+                {this.state.newReceipt ? this.renderModal() : false}
                 <MenuButton     display={this.state.display} 
                                 rotate={this.state.rotate} 
                                 onClickRotate={this.changeRotate} 
