d764054afb13926443c0b6232815d5ced64e4b8a
Felipe Hargreaves <fhargreaves00@gmail.com>
2018-10-10 23:14:04 -0300

    Wire ReceiptView with real data.
    Co-authored-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>

--- src/components/Receipt/ReceiptList/List.js
+++ src/components/Receipt/ReceiptList/List.js
@@ -7,21 +7,27 @@ import Backdrop from '../../UI/BackDrop/BackDrop'
 class ReceiptList extends React.Component {
 
 	state = {
-		showModal: false
+		showModal: false,
+		selectedReceipt: null,
 	}
 
 	render() {
+		let receiptView = null;
+
+		if (this.state.selectedReceipt) {
+			receiptView = <ReceiptView onClosePopup={this.onClosePopup} receipt={this.state.selectedReceipt} />
+		}
 
 		return (
 			<div className='container-receipts'>
 				<Backdrop show={this.state.showModal} click={this.onClosePopup} />
 
-				{this.state.showModal ? <ReceiptView onClosePopup={this.onClosePopup} /> : null}
+				{receiptView}
 
 				{this.props.receipts.map(receipt => {
 					console.log(this.props.receipts)
 					return <div key={receipt.id}>
-						<Receipt size="small" onClickHandler={this.onOpenPopup}>
+						<Receipt size="small" onClickHandler={() => this.onOpenPopup(receipt)}>
 							<div className='container-receipts__receipt-data receipt-font'>
 								<div className='dadosNotas'>
 									<div className='dados'>
@@ -46,8 +52,18 @@ class ReceiptList extends React.Component {
 		)
 	}
 
-	onOpenPopup = () => { this.setState({ showModal: true }) }
-	onClosePopup = () => { this.setState({ showModal: false }) }
+	onOpenPopup = (receipt) => {
+		this.setState({
+			showModal: true,
+			selectedReceipt: receipt
+		})
+	}
+	onClosePopup = () => {
+		this.setState({
+			showModal: false,
+			selectedReceipt: null,
+		})
+	}
 }
 
 export default ReceiptList
--- src/components/Receipt/ReceiptView/ReceiptView.js
+++ src/components/Receipt/ReceiptView/ReceiptView.js
@@ -1,42 +1,34 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './ReceiptView.scss'
 import Modal from '../../UI/Modal/Modal'
 import Receipt from '../../UI/Receipt/Receipt'
 import BaseButton from '../../UI/Button/BaseButton/BaseButton'
 
-class ReceiptView extends Component{
-  receipt = {
-    "cnpj": "00.000.000/0000-00",
-    "emission_date": "00/00/0000",
-    "emission_place": "Gama",
-    "tax_value": 20.20,
-    "total_price": 40.23
-  }
-  
-  render(){
-    return(
+class ReceiptView extends Component {
+  render() {
+    return (
       <Modal>
         <Receipt size='large'>
           <div className='receipt-area receipt-font'>
             <div className='receipt-area__content'>
               <p className="receipt-font receipt-area__content__label"><b>CNPJ:</b></p>
-              <p>{this.receipt.cnpj}</p>
+              <p>{this.props.receipt.cnpj}</p>
             </div>
             <div className='receipt-area__content'>
               <p className="receipt-font receipt-area__content__label"><b>Data:</b></p>
-              <p>{this.receipt.emission_date}</p>
+              <p>{this.props.receipt.emission_date}</p>
             </div>
             <div className='receipt-area__content'>
               <p className="receipt-font receipt-area__content__label"><b>Lugar:</b></p>
-              <p>{this.receipt.emission_place}</p>
+              <p>{this.props.receipt.emission_place}</p>
             </div>
             <div className='receipt-area__content'>
               <p className="receipt-font receipt-area__content__label"><b>Impostos:</b></p>
-              <p>{this.receipt.tax_value}</p>
+              <p>{this.props.receipt.tax_value}</p>
             </div>
             <div className='receipt-area__content'>
               <p className="receipt-font receipt-area__content__label"><b>Total:</b></p>
-              <p>{this.receipt.total_price}</p>
+              <p>{this.props.receipt.total_price}</p>
             </div>
           </div>
         </Receipt>
@@ -47,7 +39,7 @@ class ReceiptView extends Component{
             <BaseButton type="no-background" click={this.onConfirmHandler}>Editar</BaseButton>
             <BaseButton type="delete" click={this.onConfirmHandler}>Excluir</BaseButton>
           </div>
-          
+
           <BaseButton className='confirm-button' type="confirm" click={this.props.onClosePopup}>Confirmar</BaseButton>
         </div>
       </Modal>
