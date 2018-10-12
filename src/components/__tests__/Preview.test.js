e14e97948ba9f3e6aa2dfeef2138b0b366f322e0
devsalula <saleh.nazih.dev@gmail.com>
2018-10-12 19:29:11 -0300

    Test Preview
    Co-authored-by: Youssef <emaildeyoussefmuhamad@gmail.com>
    Co-authored-by: Saleh <saleh.nazih.dev@gmail.com>

---
+++ src/components/__tests__/Preview.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Preview from '../Preview/Preview'
+import ReactDOM from 'react-dom'
+
+configure({adapter: new Adapter()})
+
+describe('Testing <Preview />', () => {
+
+    let wrapper = null
+    beforeEach( () => {
+        wrapper = shallow(<Preview />)
+    })
+
+    it('it should have find preview className just once', () => {
+        expect(wrapper.find('.preview')).toHaveLength(1)
+    })
+})
