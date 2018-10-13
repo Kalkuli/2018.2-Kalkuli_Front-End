c3d8919c2e82bf31de3edef9d11f8f2917fde295
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-10-13 00:23:55 -0300

    Test Receipt
    Co-authored-by: Youssef <emaildeyoussefmuhamad@gmail.com>
    Co-authored-by: Saleh <saleh.nazih.dev@gmail.com>

---
+++ src/components/__tests__/Receipt.test.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Receipt from '../UI/Receipt/Receipt'
+
+configure({adapter: new Adapter()})
+
+describe('Testing <Receipt />', () => {
+
+  let wrapper = null
+  beforeEach(() => {
+    wrapper = shallow(<Receipt />)
+  })
+
+  it('should render follow receipt className', () => {
+    expect(wrapper.find('.receipt').exists()).toBe(true)
+  })
+
+  it('should toggle the className to large', () => {
+    wrapper.setProps({size: 'large'})
+    expect(wrapper.find('.receipt--large')).toHaveLength(1)
+  })
+
+  it('should toggle the className to small', () => {
+    wrapper.setProps({size: 'small'})
+    expect(wrapper.find('.receipt--small')).toHaveLength(1)
+  })
+})
