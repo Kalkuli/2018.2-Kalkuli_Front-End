1ab6af6f732bf72144fa745717c899a3c298efe9
devsalula <saleh.nazih.dev@gmail.com>
2018-10-13 00:28:56 -0300

    Change Component Report to the method
    Co-authored-by: Youssef <emaildeyoussefmuhamad@gmail.com>
    Co-authored-by: Saleh <saleh.nazih.dev@gmail.com>

--- src/components/UI/Report/Report.js
+++ src/components/UI/Report/Report.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Report.scss'
 import ReceiptInfo from '../ReceiptInfo/ReceiptInfo'
 
 const soma = {
     total: '81,92'
-    }
+}
 
-class Report extends Component {
-    render(){
-        return(
-            <div className="report">
-                <div className="report__content">
-                    {this.props.data.map((dados) => {
-                        return(
-                        <ReceiptInfo date={dados.date} cnpj={dados.cnpj} price={dados.price}/>
-                    );
-                    })}
-                </div>
-                <p className="report__total" >Valor total: R${soma.total}</p>
+const Report = (props) => {
+    return (
+        <div className="report">
+            <div className="report__content">
+                {props.data.map((props) => {
+                    return(
+                    <ReceiptInfo date={props.date} cnpj={props.cnpj} price={props.price}/>
+                );
+                })}
             </div>
-        )
-    }
+            <p className="report__total" >Valor total: R${soma.total}</p>
+        </div>
+    )
 }
 
 export default Report
