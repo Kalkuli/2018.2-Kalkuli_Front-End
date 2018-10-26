fa54f5905257580a23aab8d7f0b76f0fa2bb7917
EsioFreitas <esio.gustavo@gmail.com>
2018-10-25 21:24:54 -0300

    Refactory Register component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -7,6 +7,10 @@ import Button from '../UI/Button/BaseButton/BaseButton'
 
 class Register extends Component {
 
+    t = (a) =>{
+        console.log(a)
+    }
+
     render(){
         return(
             <Modal show={this.props.show}>
@@ -15,46 +19,50 @@ class Register extends Component {
                         <h1>Pronto para ter melhor gerenciamento das suas notas?!</h1>
                         <h2>Empresa:</h2>
                         <form>
-                            <div className='register__form--space'>
-                                <label for='propertyName'> 
-                                    Razão social da Empresa: 
-                                    <input id='propertyName'></input>
-                                </label>
-                                <label for='companyEmail'> 
-                                    Email da Empresa:
-                                    <input type='email' id='companyEmail'></input>
-                                </label>
-                                <label for='companyName'> 
-                                    Nome Fantasia da Empresa:
-                                    <input id='companyName'></input>
-                                </label>
-                                <label for='companyPhone'> 
-                                    Telefone Empresarial:
-                                    <input id='companyPhone'></input>
-                                </label>
-                            </div>
-                            <div>
-                                <label for='CNPJ'> 
-                                    CNPJ da Empresa:
-                                    <input id='CNPJ'></input>
-                                </label>
-                                <label for='CEP'> 
-                                    CEP da Empresa:      
-                                    <input id='CEP'></input>
-                                </label>
-                                <label for='city'> 
-                                    Cidade:
-                                    <input id='city'></input>
-                                </label>
-                                <label for='state'> 
-                                    Estado:
-                                    <input id='state'></input>
-                                </label>
+                            <label for='propertyName' className='register__form__company'> 
+                                Razão social da Empresa: 
+                                <input id='propertyName' ></input>
+                            </label>
+                            <div className='register__form--company'>  
+                                <div >
+                                    <label for='companyEmail'> 
+                                        Email da Empresa:
+                                        <input type='email' id='companyEmail'></input>
+                                    </label>
+                                    <label for='companyName'> 
+                                        Nome Fantasia da Empresa:
+                                        <input id='companyName'></input>
+                                    </label>
+                                    <label for='companyPhone'> 
+                                        Telefone Empresarial:
+                                        <input id='companyPhone'></input>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label for='CNPJ'> 
+                                        CNPJ da Empresa:
+                                        <input id='CNPJ'></input>
+                                    </label>
+                                    <label for='CEP'> 
+                                        CEP da Empresa:      
+                                        <input id='CEP'></input>
+                                    </label>
+                                    <div className='register__form__local'>
+                                        <label for='city' className='register__form__local__cidade'> 
+                                            Cidade:
+                                            <input id='city'></input>
+                                        </label>
+                                        <label for='state'> 
+                                            Estado:
+                                            <input id='state'></input>
+                                        </label>
+                                    </div>
+                                </div>
                             </div>
                         </form>
                         <h2>Administrador:</h2>
-                        <form>
-                            <div className='register__form--space'> 
+                        <form className='register__form--adm'>
+                            <div> 
                                 <label for='admName'> 
                                     Nome do Administrador:    
                                     <input id='admName'></input>
