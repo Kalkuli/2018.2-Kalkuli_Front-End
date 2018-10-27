7c30a7ad0509070f0e4973b5b450a2483284752d
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-10-27 00:00:40 -0300

    Add responsiveness to HomepageNavBar

--- src/components/UI/Navbar/HomeNavBar/HomeNavBar.js
+++ src/components/UI/Navbar/HomeNavBar/HomeNavBar.js
@@ -3,6 +3,7 @@ import './HomeNavBar.scss'
 import Scrollchor from 'react-scrollchor';
 import SignUp from '../../Button/SignUp/SignUp'
 import {Link} from 'react-router-dom'
+const smallDevice = window.matchMedia('(max-width: 720px)').matches
 
 class HomeNavBar extends Component {
 
@@ -12,7 +13,7 @@ class HomeNavBar extends Component {
 
   componentDidMount() {
     document.addEventListener('scroll', () => {
-      let isTop = window.scrollY < 550;
+      let isTop = window.scrollY < 250;
       if (isTop !== this.state.isTop) {
         this.setState({ isTop })
       }
@@ -20,24 +21,36 @@ class HomeNavBar extends Component {
   }
 
   render() {
+
     let styles = ["nav"]
     if(this.state.isTop)
       styles.push("transparent")
     else
       styles.push("color")
-    
-    return(
-      <div className={styles.join(' ')}>
-        <a><h1 className="nav__logo">Kalkuli</h1></a>
-        <nav className="nav__end">
-          <Scrollchor to="#about" className="nav__link">Sobre</Scrollchor>
-          <Scrollchor to="#features" className="nav__link">Funcionalidades</Scrollchor>
-          <div className="nav__bar"></div>
-          <Link to="/" className="nav__link" href="">Log in</Link>
-          <SignUp size="small"/>
-        </nav>
-      </div>
-    )
+    if(smallDevice) {
+      return(
+        <div className={styles.join(' ')}>
+          <a><h1 className="nav__logo">Kalkuli</h1></a>
+          <nav className="nav__end">
+            <Link to="/" className="nav__link" href="">Log in</Link>
+            <SignUp size="small"/>
+          </nav>
+        </div>
+      )
+    } else {
+      return(
+        <div className={styles.join(' ')}>
+          <a><h1 className="nav__logo">Kalkuli</h1></a>
+          <nav className="nav__end">
+            <Scrollchor to="#about" className="nav__link">Sobre</Scrollchor>
+            <Scrollchor to="#features" className="nav__link">Funcionalidades</Scrollchor>
+            <div className="nav__bar"></div>
+            <Link to="/" className="nav__link" href="">Log in</Link>
+            <SignUp size="small"/>
+          </nav>
+        </div>
+      )
+    }
   }
 }
 
