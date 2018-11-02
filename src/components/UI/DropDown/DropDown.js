e17c27febcbb9da5cfe56443e62f17531184232d
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-01 23:31:29 -0300

    Pass the color for each tag, and change the color of the selected one
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/components/UI/DropDown/DropDown.js
+++ src/components/UI/DropDown/DropDown.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import './DropDown.scss'
+import TagItem from '../TagItem/TagItem'
+
 const DropDown = (props) => {
 
   return (
     <div className="select-box">
       <div className="select-box--container" onClick={props.onDropDownHandler}>
-        <div className="select-box--selected-item">
-          { props.selectedTag.category }
+        <div className="select-box--selected-item" style={{backgroundColor: props.selectedTag.color}}>
+          { props.selectedTag.category ? props.selectedTag.category : 'Adicionar' }
         </div>
         {/* <div className="select-box--arrow" onClick={props.onDropDownHandler}>
           <span className={props.showItems ? 'select-box--arrow-up' : 'select-box--arrow-down'}/>
         </div> */}
       </div>
         
-      <div className="select-box--items" style={{display: props.showItems ? 'flex' : 'none'}}>
+      <div className="select-box--items" style={{display: props.showItems ? 'block' : 'none'}}>
         {props.items.map(item => (
-          <div key={item.id} onClick={() => props.onSelectedTagHandler(item)}>
-            {item.category}
-          </div>
+          <TagItem  key={item.id} 
+                    onClickHandler={() => props.onSelectedTagHandler(item)} 
+                    name={item.category}
+                    color={item.color}/>
         ))}
         <div>
           Criar categoria
