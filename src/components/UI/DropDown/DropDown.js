3fe3be7669ed46c43db7bcbceb9b849ad83635e6
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-02 14:52:00 -0300

    Animate selected tag, and add SearchBar
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/components/UI/DropDown/DropDown.js
+++ src/components/UI/DropDown/DropDown.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import './DropDown.scss'
 import TagItem from '../TagItem/TagItem'
-
 const DropDown = (props) => {
 
   return (
     <div className="select-box">
-      <div className="select-box--container" onClick={props.onDropDownHandler}>
-        <div className="select-box--selected-item" style={{backgroundColor: props.selectedTag.color}}>
+      <div className="select-box--container" onClick={props.onDropDownHandler} style={{backgroundColor: props.selectedTag.color}}>
+        <div className="select-box--selected-item">
           { props.selectedTag.category ? props.selectedTag.category : 'Adicionar' }
         </div>
-        {/* <div className="select-box--arrow" onClick={props.onDropDownHandler}>
-          <span className={props.showItems ? 'select-box--arrow-up' : 'select-box--arrow-down'}/>
-        </div> */}
       </div>
         
       <div className="select-box--items" style={{display: props.showItems ? 'block' : 'none'}}>
@@ -22,7 +18,7 @@ const DropDown = (props) => {
                     name={item.category}
                     color={item.color}/>
         ))}
-        <div>
+        <div className="select-box--items__create-tag">
           Criar categoria
         </div>
       </div>
---
+++ src/components/UI/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SearchBar.scss'
+
+const SearchBar = () => (
+  <div>
+    <p>
+      Pesquisar
+    </p>
+  </div>
+)
+
+export default SearchBar
