f1774884d67bdc45d64a378ed37dc7452a831124
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-02 18:11:23 -0300

    Refactor ReceiptList's logic for MenuButton into MenuButton component
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/components/Receipt/ReceiptList/ReceiptList.js
+++ src/components/Receipt/ReceiptList/ReceiptList.js
@@ -3,40 +3,25 @@ import List from './List'
 import Navbar from '../../UI/Navbar/Navbar'
 import './ReceiptList.scss'
 import MenuButton from '../../UI/Button/MenuButton/MenuButton'
-import ReceiptAdder from '../../Receipt/ReceiptAdder/ReceiptAdder'
-import Backdrop from '../../UI/BackDrop/BackDrop'
 import { connect } from 'react-redux'
 import * as actionTypes from '../../../store/actions/actions'
 import getAllReceipts from '../../../services/getAllReceipts'
-
 const smallDevice = window.matchMedia('(max-width: 800px)').matches
-
 export class ReceiptList extends Component {
-
     state = {  
         loaded: false,
-        rotate: false,
-        newReceipt: true,
-        clickedMenuButton: false
     }
-
     componentDidMount() {
         this.fetchReceipts()
     }
-
     render() {
         return (
             <div >
                 <Navbar/>
-                { this.state.newReceipt ? this.renderReceiptAdder() : null }
                 { this.state.loaded && <List    receipts={this.props.receipts} 
                                                 onGetAllReceipts={this.fetchReceipts}
                                                 isSmall={smallDevice} /> }
-                <MenuButton     rotate={this.state.rotate} 
-                                clickedMenuButton={this.state.clickedMenuButton}
-                                onClickMenuButton={this.onClickMenuButton} 
-                                onNewReceiptHandler={this.onToggleNewReceipt}
-                                onNewReportHandler={this.onNewReportHandler}/>
+                <MenuButton /> 
             </div>
         )
     }
@@ -46,38 +31,15 @@ export class ReceiptList extends Component {
         this.setState({ loaded: true })
         this.props.onReceiptsAdded(receipts)
     }
-
-    onClickMenuButton = () => { 
-        !this.state.clickedMenuButton ? this.setState({clickedMenuButton: true}) : null
-        this.setState(prevState => ({rotate: !prevState.rotate}))
-    }
-
-    renderReceiptAdder = () => {
-        return (
-            <Fragment>
-                <Backdrop show={this.state.newReceipt} click={this.onToggleNewReceipt} />
-                <ReceiptAdder onCancelHandler={this.onCloseReceiptAdder} show={this.state.newReceipt}/>
-            </Fragment>
-        )
-    }
-
-    onCloseReceiptAdder = () => { this.setState({newReceipt: false}) }
-
-    onToggleNewReceipt = () => { this.setState(prevState => ({newReceipt: !prevState.newReceipt})) }
-
-    onNewReportHandler = () => { this.props.history.push({pathname: '/reports'}) }
 }
-
 export const mapStateToProps = state => {
     return {
         receipts: state.receipts
     }
 }
-
 export const mapDispatchToProps = dispatch => {
     return {
         onReceiptsAdded: (receipts) => dispatch({type: actionTypes.ADD_RECEIPTS, receipts: receipts}) 
     }
 }
-
 export default connect(mapStateToProps, mapDispatchToProps)(ReceiptList)
--- src/components/UI/Button/MenuButton/MenuButton.js
+++ src/components/UI/Button/MenuButton/MenuButton.js
@@ -2,40 +2,72 @@ import React, {Fragment} from 'react'
 import './MenuButton.scss'
 import imgReport from '../../../../assets/img/report.svg'
 import imgReceipt from '../../../../assets/img/receiptWhite.svg'
+import ReceiptAdder from '../../../Receipt/ReceiptAdder/ReceiptAdder'
+import Backdrop from '../../BackDrop/BackDrop'
+import { withRouter } from 'react-router-dom'
+class MenuButton extends React.Component {
+    state = {
+        rotate: false,
+        newReceipt: false,
+        clickedMenuButton: false
+    }
+    render () {
+        let styleMenuButton = ["menubutton button"]
+        this.state.rotate ? styleMenuButton.push("rotate menubutton--open") : null
+       
+        let styleOptions = ["options"]
+        if(!this.state.clickedMenuButton)
+            styleOptions.push("firstTimeRunning")
+        else if(this.state.rotate)
+            styleOptions.push("open")
+        else
+            styleOptions.push("close")
+       
+        return (
+            <Fragment>
+                { this.state.newReceipt ? this.renderReceiptAdder() : null }
+                <div className={styleMenuButton.join(' ')} onClick={this.onClickMenuButton} >
+                    <h1 className="menubutton__plus base-button">+</h1>
+                </div>
+                <div className={styleOptions.join(' ')}>
+                    <div className="options__element">
+                        <p className="options__element--text">Nova Nota</p>
+                        <span className="options__element--circle button" onClick={this.onNewReceiptHandler}>
+                            <img src={imgReceipt} alt="nota fiscal"/>
+                        </span>            
+                    </div>
+                    <div className="options__element">
+                        <p className="options__element--text">Novo Relatório</p>
+                        <span className="options__element--circle button" onClick={this.onNewReportHandler}>
+                            <img src={imgReport} alt="relatorio"/>
+                        </span>
+                    </div>
+                </div> 
+            </Fragment>
+        )
+    }
 
-const MenuButton = (props) => {
-    let styleMenuButton = ["menubutton button"]
-    props.rotate ? styleMenuButton.push("rotate menubutton--open") : null
-   
-    let styleOptions = ["options"]
-    if(!props.clickedMenuButton)
-        styleOptions.push("firstTimeRunning")
-    else if(props.rotate)
-        styleOptions.push("open")
-    else
-        styleOptions.push("close")
-   
-    return (
-        <Fragment>
-            <div className={styleMenuButton.join(' ')} onClick={props.onClickMenuButton} >
-                <h1 className="menubutton__plus base-button">+</h1>
-            </div>
-            <div className={styleOptions.join(' ')}>
-                <div className="options__element">
-                    <p className="options__element--text">Nova Nota</p>
-                    <span className="options__element--circle button" onClick={props.onNewReceiptHandler}>
-                        <img src={imgReceipt} alt="nota fiscal"/>
-                    </span>            
-                </div>
-                <div className="options__element">
-                    <p className="options__element--text">Novo Relatório</p>
-                    <span className="options__element--circle button" onClick={props.onNewReportHandler}>
-                        <img src={imgReport} alt="relatorio"/>
-                    </span>
-                </div>
-            </div> 
-        </Fragment>
-    )
+    onClickMenuButton = () => { 
+        !this.state.clickedMenuButton ? this.setState({clickedMenuButton: true}) : null
+        this.setState(prevState => ({rotate: !prevState.rotate}))
+    }
+
+    onNewReceiptHandler = () => { this.setState({ newReceipt: true }) }
+
+    onToggleNewReceipt = () => { this.setState(prevState => ({newReceipt: !prevState.newReceipt})) }
+    
+    onNewReportHandler = () => { this.props.history.push({pathname: '/reports'}) }
+    
+    renderReceiptAdder = () => {
+        return (
+            <Fragment>
+                <Backdrop show={this.state.newReceipt} click={this.onToggleNewReceipt} />
+                <ReceiptAdder onCancelHandler={this.onCloseReceiptAdder} show={this.state.newReceipt}/>
+            </Fragment>
+        )
+    }
+
+    onCloseReceiptAdder = () => { this.setState({newReceipt: false}) }
 }
 
-export default MenuButton
+export default withRouter(MenuButton)
