69be7758a5375ba9abe5e9e04e288a9f035b797d
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-03 23:11:02 -0300

    Add test to Login componente
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

---
+++ src/components/__tests__/Login.test.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Login from '../Login/Login'
+
+configure({adapter: new Adapter()})
+
+describe('Testing <Login />', () => { 
+  let wrapper = null
+  let props = {
+    inputs: {
+      email: {
+        color: "#353535",
+        name: "E-mail",
+        touched: false,
+        type: "email",
+        valid: false,
+        validation: {
+          aroba: "@", required: true, minLength: 5, maxLength: 30
+        },
+        value: ""
+      },
+      senha: {
+        color: "#353535",
+        name: "Senha",
+        touched: false,
+        type: "password",
+        valid: false,
+        validation: {
+          required: true, minLength: 5, maxLength: 30
+        },
+        value: ""
+      }
+    },
+  }
+  beforeEach(() => {
+      wrapper = shallow(<Login {...props}/>)
+  })
+  it('should find login__form__inputs', () => {
+    expect(wrapper.find('.login__form__inputs').exists()).toBe(true)
+  })
+})
