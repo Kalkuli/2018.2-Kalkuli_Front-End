8db1665cfddbee5809ffae69ce143134cee354fc
phe0 <pheofo@gmail.com>
2018-11-06 18:08:08 -0200

    Finish create category page
    Co-authored-by: Pedro Féo <pheofo@gmail.com>
    Co-authored-by: Saleh Nazih <saleh.nazih.dev@gmail.com>

---
+++ src/components/UI/Colors/Colors.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react'
+import './Colors.scss'
+import BaseButton from '../Button/BaseButton/BaseButton';
+
+const colors = [{
+    color: '#FF4F78'
+},
+{
+    color: '#8037FF'
+},
+{
+    color: '#00DF6B'
+},
+{
+    color: '#E045C1'
+},
+{
+    color: '#00CFDA'
+},
+{
+    color: '#FFA312'
+},
+{
+    color: '#005D27'
+},
+{
+    color: '#451259'
+},
+{
+    color: '#d8bd22'
+},
+{
+    color: '#3951b2'
+}]
+
+class Colors extends Component {
+    state = {
+        value: '',
+        selected: null
+    }
+    render(){
+        return(
+            <div className='create-category'>
+                <p className='create-category__phrase'>Nome:</p>
+                <input className='create-category__input' value={this.state.value} onChange={this.handleChange} />
+                <p className='create-category__phrase'>Selecione uma cor:</p>
+                <div className='create-category__colors'>
+                    {colors.map( (color, index) => {
+                        var colorClass = index === this.state.selected ? 'create-category__colors__color2' : 'create-category__colors__color'
+                        return(
+                            <div className={colorClass} onClick={() => this.clickColor(index)} style={{backgroundColor: color.color}}></div>
+                            )
+                        }, )
+                    }
+                </div>
+                <div className='create-category__buttons'>
+                    <BaseButton type={'delete'} size={'small'}>Cancelar</BaseButton>
+                    <BaseButton type={'confirm'} size={'small'}>Confirmar</BaseButton>
+                </div>
+            </div>
+        )
+    }
+
+    clickColor = (index) => {
+        this.setState({selected: index})
+    } 
+
+    handleChange = (event) => {
+        this.setState({value: event.target.value});
+    }
+}
+
+export default Colors
