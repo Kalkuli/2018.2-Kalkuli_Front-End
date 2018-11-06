00e64fd801f8ba63fbc0b3fb780ba8df6034aaaa
phe0 <pheofo@gmail.com>
2018-11-06 11:10:55 -0200

    Fix responsiviness on report saved tags

--- src/components/UI/DropDown/DropDown.js
+++ src/components/UI/DropDown/DropDown.js
@@ -2,26 +2,30 @@ import React from 'react'
 import './DropDown.scss'
 import TagItem from '../TagItem/TagItem'
 import SearchBar from '../SearchBar/SearchBar'
+
 const DropDown = (props) => {
   return (
     <div className="select-box">
+
       <div className="select-box--container" onClick={props.onDropDownHandler} style={{backgroundColor: props.selectedTag.color}}>
         <div className="select-box--selected-item">
           { props.selectedTag.category ? props.selectedTag.category : 'Adicionar' }
         </div>
       </div>
+      
       <div className="select-box--items" style={{display: props.showItems ? 'block' : 'none'}}>
         <SearchBar />
+        
         {props.items.map(item => (
           <TagItem  key={item.id} 
                     onClickHandler={props.onSelectedTagHandler.bind(this, item)} 
                     name={item.category}
                     color={item.color}/>
         ))}
-        <div className="select-box--items__create-tag">
-          Criar categoria
-        </div>
+
+        <div className="select-box--items__create-tag">Criar categoria</div>
       </div>
+
     </div>
   )
 }
