3cc5798cfb37e61d48a57db01c3943ec0bff04d2
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-14 01:09:06 -0200

    Test DropArea with 100% coverage

---
+++ src/components/__tests__/DropArea.test.js
@@ -0,0 +1,19 @@
+import DropArea from '../UI/DropArea/DropArea'
+import ReactDropzone from 'react-dropzone'
+
+describe('Testing <DropArea/>', () => {
+
+  let wrapper = null
+  beforeEach(() => {
+    wrapper = shallow(<DropArea/>)
+  })
+
+  it('should find ReactDropzone', () => {
+    expect(wrapper.find(ReactDropzone).exists()).toBe(true)
+  })
+
+  it('should change message when file is selected', () => {
+    wrapper.setProps({fileSelected: true})
+    expect(wrapper.html()).toMatch('Arquivo Selecionado com Sucesso')
+  })
+})
