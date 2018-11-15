2eb3487da2ab45ea71b7ba541573e1e77790dc45
Dutra <ldutra98@gmail.com>
2018-11-15 14:21:31 -0200

    Adjust tags requests to fit users restrictions
    Co-authored-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>

--- src/services/getAllTags.js
+++ src/services/getAllTags.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import * as baseUrl from '../helpers/baseUrl'
+import {config, baseURL} from './axiosConfig'
 
 export default async (mockError) => {
   let response = null
+  const company_id = localStorage.getItem('company_id')
   try {
-    if(mockError) throw 'mockinError'
-    response = await axios.get(baseUrl.default + '/api/v1/tags')
+    if(mockError) throw 'mockingError'
+    response = await axios.get(`${baseUrl.default}/${company_id}/tags`, config)
     return response.data.data.tags
   } catch(err) {
     return response
--- src/services/newTag.js
+++ src/services/newTag.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 import * as baseUrl from '../helpers/baseUrl'
+import {config, baseURL} from './axiosConfig'
 
 export default async (tag, mockError) => {
     try {
         if(mockError) throw 'mockingError'
-        await axios.post(baseUrl.default + '/api/v1/create_tag', {"tag": tag })
+        await axios.post(`${baseUrl.default}/create_tag`, {"tag": tag }, config)
         return 'success'
     }
     catch(err){
