704759206019d3d75db9afa323e195df2f965cd7
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-16 18:34:44 -0200

    Test /helpers with 100% coverage

---
+++ src/components/__tests__/screenSize.test.js
@@ -0,0 +1,7 @@
+import * as screenSize from '../../helpers/screenSize'
+
+it('should check screen orientation', () => {
+  expect(screenSize.HORIZONTAL_ORIENTATION).toMatch('horizontal')
+  expect(screenSize.VERTICAL_ORIENTATION).toMatch('vertical')
+  expect(screenSize.VERTICAL_SCROLLABLE).toMatch('verticalScrollable')
+})
