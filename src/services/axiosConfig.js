8bb0074071182202b895564a02bcb7c159238a71
EsioFreitas <esio.gustavo@gmail.com>
2018-11-16 13:25:20 -0200

    Add homologation route
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -3,4 +3,4 @@ const token = localStorage.getItem('auth_token')
 export const config = {
   headers: {'Authorization': "Bearer " + token}
 }
-export const baseURL = 'http://172.22.0.1:5008/api/v1'
+export const baseURL = 'https://2wpulxi1r7.execute-api.sa-east-1.amazonaws.com/hom/api/v1'
