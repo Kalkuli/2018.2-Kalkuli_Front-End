4ee068b98321f991be36b744d8a68457fda25d40
EsioFreitas <esio.gustavo@gmail.com>
2018-11-16 13:12:56 -0200

    Add condirmation message to update a receipt
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>

--- src/services/updateReceipt.js
+++ src/services/updateReceipt.js
@@ -2,9 +2,13 @@ import axios from 'axios'
 import {config, baseURL} from './axiosConfig'
 
 export default async (receipt_id, receipt) => {
-
-  console.log(receipt)
   const company_id = localStorage.getItem('company_id')
+  let response = 'done'
   await axios.put(`${baseURL}/${company_id}/update_receipt/${receipt_id}`, receipt, config)
-  return "Success"
+  .catch(err =>{
+    response = 'error'
+    return response
+  })
+
+  return response
 }
