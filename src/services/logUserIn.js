3e0875071373ff09640fd89b1e2a951af14255f0
EsioFreitas <esio.gustavo@gmail.com>
2018-11-16 14:06:37 -0200

    Add homologation route
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>

--- src/services/logUserIn.js
+++ src/services/logUserIn.js
@@ -5,8 +5,11 @@ export default async (user, mockError) => {
   let response = null
   try {
     if(mockError) throw 'mockingError'
-    response = await axios.post(baseUrl.default + '/api/v1/auth/login', user)
-    return response.data.auth_token
+    response = await axios.post(`&{baseUrl.default}/api/v1/auth/login`, user)
+    return {
+      token: response.data.auth_token,
+      company_id: response.data.company_id
+    }
   } catch(err) {
     return 'error'
   }
