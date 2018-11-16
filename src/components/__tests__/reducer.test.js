f61976975a4f9d90a046d537ba1b697df1039f12
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-16 19:48:02 -0200

    Test reducer for updating store when adding filePDF

--- src/components/__tests__/reducer.test.js
+++ src/components/__tests__/reducer.test.js
@@ -14,4 +14,12 @@ describe('Testing reducers', () => {
   it('should return the initial state', () => {
     expect(reducer(undefined, {})).toEqual(initialState)
   })
+
+  it('should return the state with the added PDF File', () => {
+    const action = {
+      type: actionTypes.ADD_PDF_FILE,
+      filePDF: 'test'
+    }
+    expect(reducer(initialState, action)).toEqual({...initialState, filePDF: action.filePDF})
+  })
 })
