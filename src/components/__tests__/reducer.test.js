6fb6ba9ac41027ae1d07f7e6ecfc87f75ddf3d8c
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-16 19:51:43 -0200

    Test reducer for updating store when adding receipt array

--- src/components/__tests__/reducer.test.js
+++ src/components/__tests__/reducer.test.js
@@ -22,4 +22,17 @@ describe('Testing reducers', () => {
     }
     expect(reducer(initialState, action)).toEqual({...initialState, filePDF: action.filePDF})
   })
+
+  it('should return the state with the added receipt array', () => {
+    const action = {
+      type: actionTypes.ADD_RECEIPTS,
+      receipts: [{
+        title: 'Nota 10',
+        tag_id: 1,
+        description: 'testing'
+      }]
+    }
+    expect(reducer(initialState, action)).toEqual({...initialState, receipts: action.receipts})
+
+  })
 })
