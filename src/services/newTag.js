288214a4309443583898926e0db15eb977f727bf
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-17 01:00:47 -0200

    Test newTag request with 100% coverage

--- src/services/newTag.js
+++ src/services/newTag.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
 
-export default async (tag) => {
+export default async (tag, mockError) => {
     try {
+        if(mockError) throw 'mockingError'
         await axios.post('https://2wpulxi1r7.execute-api.sa-east-1.amazonaws.com/hom/api/v1/create_tag', {"tag": tag })
         return 'success'
     }
     catch(err){
-        return err.response.data.message
+        return 'error'
     }
 }
