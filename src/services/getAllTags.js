292817389e1bcf890848b68eb2b20c19c98c161e
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-17 00:34:34 -0200

    Test getAllTags request(try catch example), and simulating error to test catch statement

--- src/services/getAllTags.js
+++ src/services/getAllTags.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
-export default async () => {
+export default async (mockError) => {
   let response = null
   try {
+    if(mockError) throw 'mockinError'
     response = await axios.get('https://2wpulxi1r7.execute-api.sa-east-1.amazonaws.com/hom/api/v1/tags')
     return response.data.data.tags
   } catch(err) {
