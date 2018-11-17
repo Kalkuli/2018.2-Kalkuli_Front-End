39e345f24d93f2b4db9d6653df2d00c7fdeeb5c1
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-17 00:41:45 -0200

    Test logUserIn request try and catch with 100% of coverage

--- src/services/logUserIn.js
+++ src/services/logUserIn.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import * as baseUrl from '../helpers/baseUrl'
 
-export default async (user) => {
+export default async (user, mockError) => {
   let response = null
   try {
+    if(mockError) throw 'mockingError'
     response = await axios.post(baseUrl.default + '/api/v1/auth/login', user)
     return response.data.auth_token
   } catch(err) {
