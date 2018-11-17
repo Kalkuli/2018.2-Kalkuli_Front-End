31204f5b952d5942a81222670f920c913b1c4d96
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-16 23:28:53 -0200

    Mocking axios methods with jest.fn() to mockImplementationOnce for testing /services

--- src/components/__tests__/ReportLabels.test.js
+++
@@ -1,9 +0,0 @@
-import ReportLabels from '../UI/ReportLabels/ReportLabels';
-
-describe('Testing <ReportLabels />', () => {
-
-    it('should have found labels in report', () => {
-        const wrapper = shallow(<ReportLabels/>)
-        expect(wrapper.find('.report-labels')).toHaveLength(1)
-    })
-})
