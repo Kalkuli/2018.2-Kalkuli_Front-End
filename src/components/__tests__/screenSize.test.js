34c1a2f9551d5d2e841424c151cdf20d8c6a18c0
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-16 23:28:53 -0200

    Mocking axios methods with jest.fn() to mockImplementationOnce for testing /services

--- src/components/__tests__/screenSize.test.js
+++
@@ -1,7 +0,0 @@
-import * as screenSize from '../../helpers/screenSize'
-
-it('should check screen orientation', () => {
-  expect(screenSize.HORIZONTAL_ORIENTATION).toMatch('horizontal')
-  expect(screenSize.VERTICAL_ORIENTATION).toMatch('vertical')
-  expect(screenSize.VERTICAL_SCROLLABLE).toMatch('verticalScrollable')
-})
