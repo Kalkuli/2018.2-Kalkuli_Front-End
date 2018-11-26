dc9b550239f22bf400b1d3ec9ddd94ccbd43a0fe
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-26 21:21:43 -0200

    Test Dashboard with 87%

--- src/components/__tests__/components/Dashboard.test.js
+++ src/components/__tests__/components/Dashboard.test.js
@@ -32,34 +32,23 @@ describe('Testing <Dashboard />', () => {
         }]
     }
     const orderedReceipts = [{
-            emission_date: '2018-10-09',
-            total_price: 10.00
-        },
-        {
-            emission_date: '2018-10-09'   ,
-            total_price: 10.00
-        },
-        {
-            emission_date: '2018-10-10',
-            total_price: 10.00
-        },
-        {
-            emission_date: '2018-10-11',
-            total_price: 10.00
-        }]
-
-    const filteredReceipts = [{
         emission_date: '2018-10-09',
         total_price: 10.00
     },
     {
-        emission_date: '2018-10-09'   ,
+        emission_date: '2018-10-09',
         total_price: 10.00
     },
     {
         emission_date: '2018-10-10',
         total_price: 10.00
+    },
+    {
+        emission_date: '2018-10-11',
+        total_price: 10.00
     }]
+
+    
 
     const prices = [20.00, 10.00, 10.00]
     const series = [{
@@ -79,57 +68,57 @@ describe('Testing <Dashboard />', () => {
 
     it('should test mapStateToProps for retrieving receipts adn tags', () => {
         const initialState = {
-          receipts: [{
-              title: 'oi'
-          }],
-          tags: [{colors: 'red'}]
+            receipts: [{
+                title: 'oi'
+            }],
+            tags: [{colors: 'red'}]
         }
         expect(mapStateToProps(initialState).receipts).toEqual([{title: 'oi'}])
         expect(mapStateToProps(initialState).tags).toEqual([{colors: 'red'}])
-      })
+    })
 
-      it('should test mapDispatchToProps for dispatching onReceiptsAdded', () => {
+    it('should test mapDispatchToProps for dispatching onReceiptsAdded', () => {
         const dispatch = jest.fn()
         mapDispatchToProps(dispatch).onReceiptsAdded()
         expect(dispatch.mock.calls[0][0]).toEqual({type: 'ADD_RECEIPTS'})
-      })
+    })
 
-      it('should test mapDispatchToProps for dispatching onTagsAdded', () => {
+    it('should test mapDispatchToProps for dispatching onTagsAdded', () => {
         const dispatch = jest.fn()
         mapDispatchToProps(dispatch).onTagsAdded()
         expect(dispatch.mock.calls[0][0]).toEqual({type: 'ADD_TAGS'})
-      })
+    })
 
-      it('should order receipts by date', () => {
+    it('should order receipts by date', () => {
         wrapper.setState({ receipts: null })
         instance.organizeData()
         wrapper.setState({ receipts: orderedReceipts })
         expect(wrapper.state('receipts')).toMatchObject(orderedReceipts)
-      })
+    })
 
-      it('should sum receipts of same date', () => {
+    it('should sum receipts of same date', () => {
         instance.sumSameDate(orderedReceipts)
         expect(wrapper.state('series')).toMatchObject(series)
-      })
+    })
 
-      it('should test onConfirmHandler', () => {
+    it('should test onConfirmHandler', () => {
         instance.onConfirmHandler()
         expect(historyMock.push.mock.calls[0]).toEqual(['/confirmation']);
-      })
+    })
 
-      it('should test chooseButton', () => {
+    it('should test chooseButton', () => {
         const loading = true
         const isValid = true
         const chooseButtonLoader = shallow(instance.chooseButton(loading, isValid, props.receipts))
         expect(chooseButtonLoader).toHaveLength(1)
-      })
+    })
 
-      it('should test sumReceipts', () => {
+    it('should test sumReceipts', () => {
           instance.sumReceipts(orderedReceipts)
           expect(wrapper.state('sum')).toEqual('40.00')
-      })
+    })
 
-      it('should test onChange', () => {
+    it('should test onChange', () => {
         wrapper.setState({receipts: orderedReceipts})
         const spyOrganizeData = jest.spyOn(instance, 'organizeData')
        
@@ -140,6 +129,27 @@ describe('Testing <Dashboard />', () => {
 
         instance.onChange({startDate, endDate})
         expect(spyOrganizeData).toHaveBeenCalled()
-      })
+    })
 
+    it('should load DropDown with tags from Redux', () => {
+        wrapper.setProps({ tags: [{ id: 1, category: 'Food', color: '#424242'}] })
+        instance.handleExceptionDropDown()
+    })
+
+    it('should toggle DropDown', () => {
+        wrapper.setState({ showItems: false })
+        instance.onDropDownHandler()
+        expect(wrapper.state('showItems')).toBe(true)
+    })
+
+    it('should set the selected tag', () => {
+        const tag = {id: 1, category: 'Food', colod: 'red'}
+        instance.onSelectedTagHandler(tag)
+        expect(wrapper.state('selectedTag')).toEqual(tag)
+    })
+
+    it('should create report', () => {
+        instance.onConfirmButton()
+        expect(localStorage.getItem).toHaveBeenCalled()
+    })
 })
