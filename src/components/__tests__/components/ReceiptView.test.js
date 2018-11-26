867e4bebe10a4d594cafae56b0f27e3bf054ad1d
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-26 21:43:22 -0200

    Test ReceiptView with 86%

--- src/components/__tests__/components/ReceiptView.test.js
+++ src/components/__tests__/components/ReceiptView.test.js
@@ -3,6 +3,7 @@ import ReceiptView from '../../Receipt/ReceiptView/ReceiptView'
 import BackDrop from '../../UI/BackDrop/BackDrop'
 import BaseButton from '../../UI/Button/BaseButton/BaseButton'
 jest.mock('../../../services/deleteReceipt')
+jest.mock('../../../services/updateReceipt')
 
 describe("Testing <ReceiptView />", () => {
 
@@ -19,9 +20,11 @@ describe("Testing <ReceiptView />", () => {
 
   const spyOnClosePopUp = jest.fn()
   const spyOnGetAllReceipts = jest.fn()
+  const spyDone = jest.fn()
   const props = {
     onClosePopup: spyOnClosePopUp,
-    onGetAllReceipts: spyOnGetAllReceipts
+    onGetAllReceipts: spyOnGetAllReceipts,
+    done: spyDone
   }
 
   beforeEach(() => {
@@ -56,5 +59,45 @@ describe("Testing <ReceiptView />", () => {
     const spyOnDeleteHandler = jest.fn()
     wrapper.instance().onDeleteHandler()
     expect(wrapper.state('confirmation')).toBe(false)
+  })
+
+  it('should change state with new input value', () => {
+    const event = {
+      target: {
+        value: 'oi'
+      }
+    }
+    wrapper.setState({ receipt })
+    wrapper.instance().changeHandler(event, 'cnpj')
+  })
+
+  it('should set save state to false', () => {
+    wrapper.setState({ save: true })
+    wrapper.instance().onCancelHandlerSave()
+    expect(wrapper.state('save')).toBe(false)
+  })
+
+  it('should set edit to true', () => {
+    wrapper.setState({ edit: false })
+    wrapper.instance().onCancelHandlerCancel()
+    expect(wrapper.state('edit')).toBe(true)
+  })
+
+  it('should update the receipt', async() => {
+    wrapper.setProps({ receiptId: 1 })
+    const response = await wrapper.instance().onConfirmEdit()
+    expect(spyDone).toHaveBeenCalled()
+    expect(spyOnClosePopUp).toHaveBeenCalled()
+    expect(spyOnGetAllReceipts).toHaveBeenCalled()
+  })
+
+  it('should test onEditHandler', () => {
+    wrapper.instance().onEditHandler()
+    expect(wrapper.state('edit')).toBe(true)
+  })
+
+  it('should test onCancelEditHandler', () => {
+    wrapper.instance().onCancelEditHandler()
+    expect(wrapper.state('lastReceipt')).toBe(null)
   })
 })
