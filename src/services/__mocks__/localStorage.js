e21b670775a7f03ab728a168da53aa6831c98394
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-26 19:46:17 -0200

    Test NavBar with 100% coverage

--- src/services/__mocks__/localStorage.js
+++ src/services/__mocks__/localStorage.js
@@ -2,6 +2,6 @@ export default new class {
   store = {};
   setItem = (key, val) => (this.store[key] = val);
   getItem = key => this.store[key];
-  removeItem = key => { delete this.store[key]; };
+  removeItem = jest.fn(key => { delete this.store[key]; });
   clear = () => (this.store = {});
 }();
