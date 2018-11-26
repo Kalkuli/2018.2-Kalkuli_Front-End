bac48c6fdbbc9ff941fed12f05f38bb282971b48
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-11-26 18:40:08 -0200

    Test getAllReports request with 100% coverage

--- src/services/getAllReports.js
+++ src/services/getAllReports.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import {config, baseURL} from './axiosConfig'
 
-export default async () => {
+export default async (mockError) => {
     try {
+        if(mockError) throw new Error('mockingError')
         const company_id = localStorage.getItem('company_id')
         const response = await axios.get(`${baseURL}/${company_id}/get_all_reports`, config)
-
         return response.data.data.reports
     }
     catch(err) {
